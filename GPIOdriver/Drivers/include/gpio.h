c8fb20a3d5e742ace2c1505bf1accc64bf85dc92
Abbas Mohamed Abbas <61335191+abbas695@users.noreply.github.com>
2022-05-24 21:56:36 +0200

    Update gpio.h

--- GPIOdriver/Drivers/include/gpio.h
+++ GPIOdriver/Drivers/include/gpio.h
@@ -2,7 +2,7 @@
  * gpio.h
  *
  *  Created on: Apr 30, 2022
- *      Author: moham
+ *      Author: abbas
  */
 
 #ifndef INCLUDE_GPIO_H_
@@ -13,8 +13,4 @@ char GPIO_Write(GPIO_TypeDef * GPIOx,char pin,char state);
 void GPIO_pull_up_down_mode (GPIO_TypeDef * GPIOx,char pin,char state);
 uint32_t GPIO_read (GPIO_TypeDef * GPIOx,char pin);
 int debounce(GPIO_TypeDef * GPIOx,char pin);
-void KeyPad_INIT();
-void KeyPad_MANAGE();
-char KeyPad_GET_VALUE();
-void KeypadCallouts_KeyPressNotificaton();
 #endif /* INCLUDE_GPIO_H_ */
