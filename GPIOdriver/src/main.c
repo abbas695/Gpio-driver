ac8d4de8e5858924b0cb4b2953296414a8192e4c
Abbas Mohamed Abbas <61335191+abbas695@users.noreply.github.com>
2022-05-24 21:55:03 +0200

    Update main.c

--- GPIOdriver/src/main.c
+++ GPIOdriver/src/main.c
@@ -5,10 +5,60 @@
 
 int main(void)
 {
-	KeyPad_INIT();
+	GPIO_Init(GPIOB,5,1,1);
+	GPIO_Init(GPIOB,6,1,1);
+	GPIO_Init(GPIOB,7,1,1);
+	GPIO_Init(GPIOB,3,0,1);
+	GPIO_pull_up_down_mode(GPIOB,3,1);
+
+	// super loop
+	int count=1;
 	while(1)
 	{
-		KeyPad_MANAGE();
+		if (debounce(GPIOB,3)){
+
+			switch(count){
+
+			case 0:
+				GPIO_Write(GPIOB,7,0);
+				GPIO_Write(GPIOB,6,0);
+				GPIO_Write(GPIOB,5,0);
+				count=count + 1;
+				break;
+			case 1:
+				GPIO_Write(GPIOB,5,1);
+				count=count + 1;
+				break;
+			case 2:
+				GPIO_Write(GPIOB,6,1);
+				GPIO_Write(GPIOB,5,0);
+				count=count + 1;
+				break;
+			case 3:
+				GPIO_Write(GPIOB,5,1);
+				count=count + 1;
+				break;
+			case 4:
+				GPIO_Write(GPIOB,7,1);
+				GPIO_Write(GPIOB,6,0);
+				GPIO_Write(GPIOB,5,0);
+				count=count + 1;
+				break;
+			case 5:
+				GPIO_Write(GPIOB,5,1);
+				count=count + 1;
+				break;
+			case 6:
+				GPIO_Write(GPIOB,6,1);
+				GPIO_Write(GPIOB,5,0);
+				count=count + 1;
+				break;
+			case 7:
+				GPIO_Write(GPIOB,5,1);
+				count=0;
+				break;
+			}
+		}
 
 	}
 
